f0048b82a1d5c32252e4745d36fb1516461e224a
ajhilabs <anal.jyoti@hilabs.com>
2025-11-12 02:34:16 +0530

    Added the try catch to debugg the failing of the sendEmail functionality on render

--- src/main/java/com/analjyoti/portfolio/service/EmailService.java
+++ src/main/java/com/analjyoti/portfolio/service/EmailService.java
@@ -23,36 +23,47 @@ public class EmailService {
     }
 
     public void sendContactEmail(ContactRequest contactRequest) {
+        logger.info("Attempting to send email to: {}", recipientEmail);
+
+        SimpleMailMessage message = buildEmailMessage(contactRequest);
+        
+        logger.info("Sending email with subject: {}", message.getSubject());
+        
+        sendEmail(message);
+        
+        logger.info("Email sent successfully to: {}", recipientEmail);
+    }
+
+    private SimpleMailMessage buildEmailMessage(ContactRequest contactRequest) {
+        SimpleMailMessage message = new SimpleMailMessage();
+
+        message.setTo(recipientEmail);
+        message.setFrom(contactRequest.getEmail());
+        message.setReplyTo(contactRequest.getEmail());
+        message.setSubject("Portfolio Contact: " + contactRequest.getSubject());
+
+        String emailBody = String.format(
+            "New contact form submission from your portfolio:\n\n" +
+            "Name: %s\n" +
+            "Email: %s\n" +
+            "Subject: %s\n\n" +
+            "Message:\n%s\n\n" +
+            "---\n" +
+            "This email was sent from your portfolio contact form.",
+            contactRequest.getName(),
+            contactRequest.getEmail(),
+            contactRequest.getSubject(),
+            contactRequest.getMessage()
+        );
+
+        message.setText(emailBody);
+        
+        return message;
+    }
+
+    private void sendEmail(SimpleMailMessage message) {
         try {
-            logger.info("Attempting to send email to: {}", recipientEmail);
-
-            SimpleMailMessage message = new SimpleMailMessage();
-
-            message.setTo(recipientEmail);
-            message.setFrom(contactRequest.getEmail());
-            message.setReplyTo(contactRequest.getEmail());
-            message.setSubject("Portfolio Contact: " + contactRequest.getSubject());
-
-            String emailBody = String.format(
-                "New contact form submission from your portfolio:\n\n" +
-                "Name: %s\n" +
-                "Email: %s\n" +
-                "Subject: %s\n\n" +
-                "Message:\n%s\n\n" +
-                "---\n" +
-                "This email was sent from your portfolio contact form.",
-                contactRequest.getName(),
-                contactRequest.getEmail(),
-                contactRequest.getSubject(),
-                contactRequest.getMessage()
-            );
-
-            message.setText(emailBody);
-
             mailSender.send(message);
-
-            logger.info("Email sent successfully to: {}", recipientEmail);
-
         } catch (Exception e) {
             logger.error("Failed to send email. Recipient: {}, Error: {}", recipientEmail, e.getMessage(), e);
             throw new RuntimeException("Failed to send email: " + e.getMessage(), e);
